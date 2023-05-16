3c71fdc7a1a36fd8cbe22e91f2cde046893d905e
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-05-16 13:37:19 +0200

    fixed bug

--- chessout-app/src/app/page.tsx
+++ chessout-app/src/app/page.tsx
@@ -1,6 +1,8 @@
-import Image from "next/image";
+//import Image from "next/image";
 import { Fragment } from "react";
+
 import classes from "./Header.module.css";
+
 
 export default function Home() {
   return (
@@ -9,9 +11,9 @@ export default function Home() {
         <button>Home</button>
         <button>About</button>
       </header>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hello Chessout</div>
-    </main>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div>Hello Chessout</div>
+      </main>
     </Fragment>
   );
 }
