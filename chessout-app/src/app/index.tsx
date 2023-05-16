5b2036a43dfc2701e035c0d8f095ef37cc218eb8
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-05-16 14:55:15 +0200

    added chakra/react and chakra/theme

---
+++ chessout-app/src/app/index.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+function App() {
+  // 2. Wrap ChakraProvider at the root of your app
+  return (
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  );
+}
--- chessout-app/src/app/page.tsx
+++ chessout-app/src/app/page.tsx
@@ -1,8 +1,3 @@
-//import Image from "next/image";
-import { Fragment } from "react";
-
-import classes from "./Header.module.css";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
