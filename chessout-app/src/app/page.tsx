fbf1e774500afe1ef9d069675767b0c9d69aeaca
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-05-16 11:47:15 +0200

    added bar

--- chessout-app/src/app/page.tsx
+++ chessout-app/src/app/page.tsx
@@ -1,19 +1,17 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { Fragment } from "react";
+import classes from "./Header.module.css";
 
 export default function Home() {
   return (
+    <Fragment>
+      <header className={classes.header}>
+        <button>Home</button>
+        <button>About</button>
+      </header>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
       <div>Hello Chessout</div>
-
-      
-
-      
-
-      
-
-        
-         
     </main>
-  )
+    </Fragment>
+  );
 }
