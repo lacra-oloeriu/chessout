8e6b36498d2b6549e9e39b17f97341724134e6ff
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-05-16 19:40:55 +0200

    reverted to js react app tempalte

---
+++ chessout-app/src/App.js
@@ -0,0 +1,16 @@
+//import logo from "./logo.svg";
+//import "./App.css";
+//import  chessImage from './assets/chess-out.jpg';
+import React from "react";
+import { Fragment } from "react";
+import Header from "./Layout/Header";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
