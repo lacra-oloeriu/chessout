6d1457ca4535021671bdf0bdb8feb92fc33c4463
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-05-16 13:56:04 +0200

    removed Fragment

--- chessout-app/src/app/page.tsx
+++ chessout-app/src/app/page.tsx
@@ -3,17 +3,10 @@ import { Fragment } from "react";
 
 import classes from "./Header.module.css";
 
-
 export default function Home() {
   return (
-    <Fragment>
-      <header className={classes.header}>
-        <button>Home</button>
-        <button>About</button>
-      </header>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div>Hello Chessout</div>
-      </main>
-    </Fragment>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>Hello Chessout</div>
+    </main>
   );
 }
