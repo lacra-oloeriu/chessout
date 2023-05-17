d06a87dcf97f0ba4490a3c7c6ea476ea37e9c107
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-05-17 16:05:48 +0200

    added colorMode

--- chessout-app/src/components/NavBar.js
+++ chessout-app/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Heading, Flex, Link } from "@chakra-ui/core";
+import { Box, Heading, Flex, Link,useColorMode , IconButton} from "@chakra-ui/core";
 
 const MenuItems = ({ children }) => (
   <Link mt={{ base: 4, md: 0 }} mr={6} display="block">
@@ -8,6 +8,7 @@ const MenuItems = ({ children }) => (
 );
 
 const Navbar = (props) => {
+  const { colorMode, toggleColorMode } = useColorMode(); 
   return (
     <Flex
       as="nav"
@@ -15,10 +16,15 @@ const Navbar = (props) => {
       justify="space-between"
       wrap="wrap"
       padding="0.3rem"
-      bg="gray.900"
-      color="teal.300"
-      borderBottom="1px solid black"
-      {...props}
+      bg=
+      //color="teal.300"
+      //borderBottom="1px solid black"
+      //{...props}
+      {colorMode === "light" ? "gray.900" : "teal.500"}  
+            color=
+            {colorMode === "light" ? "teal.300" : "white"}  
+            borderBottom="1px solid black"  
+            {...props}  
     >
       <Flex align="center" mr={5}>
         <Heading as="h1" size="lg" letterSpacing={"-.1rem"}>
@@ -31,13 +37,24 @@ const Navbar = (props) => {
         width="auto"
         alignItems="center"
         flexGrow={1}
-        color="teal.300"
+        color={colorMode === "light" ? "teal.300" : "white"}
       >
         <MenuItems>Home</MenuItems>
-        <MenuItems>Blogs</MenuItems>
         <MenuItems>About</MenuItems>
         <MenuItems>Contact</MenuItems>
       </Box>
+      <Box  
+                display="block"  
+                mt={{ base: 4, md: 0 }}  
+            >  
+                <IconButton  
+                    bg="transparent"  
+                    aria-label="toggle color mode"  
+                    icon={colorMode === "light" ? "moon" : "sun"}  
+                    onClick={toggleColorMode}  
+                    color="white"  
+                />  
+            </Box>  
     </Flex>
   );
 };
--- chessout-app/src/index.js
+++ chessout-app/src/index.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import React from 'react';
+//import ReactDOM from 'react-dom/client';
+//import './index.css';
+//import App from './App';
+//import reportWebVitals from './reportWebVitals';
+//import { theme, ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core"; 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(
+ // <React.StrictMode>
+  //  <App />
+    
+ // </React.StrictMode>
+//);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
 
+import React from 'react';  
+import ReactDOM from 'react-dom';  
+import './index.css';  
+import App from './App';  
+//import * as serviceWorker from './serviceWorker';  
+import { theme, ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core";  
+  
+  
+ReactDOM.render(  
+  <ThemeProvider theme={theme}>  
+    <ColorModeProvider>  
+      <CSSReset />  
+      <React.StrictMode>  
+        <App />  
+      </React.StrictMode>  
+    </ColorModeProvider>  
+  </ThemeProvider>,  
+  document.getElementById('root')  
+);  
+  
+//serviceWorker.unregister();  
