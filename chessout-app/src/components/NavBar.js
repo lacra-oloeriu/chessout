53292a4806ce1e4ede8dd5d175ff58e6a119396b
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-06-04 08:56:19 +0200

    added a new color  to rhe button

--- chessout-app/src/components/NavBar.js
+++ chessout-app/src/components/NavBar.js
@@ -17,6 +17,8 @@ const MenuItems = ({ children }) => (
 
 const Navbar = (props) => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const bgColor = { light: "red.200", dark: "red.800" };
+  const textColor = { light: "red.800", dark: "white" };
   return (
     <Flex
       as="nav"
@@ -24,14 +26,14 @@ const Navbar = (props) => {
       justify="space-between"
       wrap="wrap"
       padding="0.3rem"
-      bg=//{...props} //borderBottom="1px solid black" //color="teal.300"
-      {colorMode === "light" ? "gray.900" : "teal.500"}
+      bg={ //{...props} //borderBottom="1px solid black" //color="teal.300"
+        colorMode === "light" ? "gray.900" : "teal.500"
+      }
       color={colorMode === "light" ? "teal.300" : "white"}
       borderBottom="1px solid black"
       {...props}
     >
       <Flex align="center" mr={5}>
-        
         <Heading as="h1" size="lg" letterSpacing={"-.1rem"}>
           Chessout
         </Heading>
@@ -49,11 +51,13 @@ const Navbar = (props) => {
       </Box>
       <Box display="block" mt={{ base: 4, md: 0 }}>
         <IconButton
-          bg="transparent"
-          aria-label="toggle color mode"
+          //bg="transparent"
+          //aria-label="toggle color mode"
           icon={colorMode === "light" ? "moon" : "sun"}
           onClick={toggleColorMode}
-          color="white"
+          //color="white"
+          color={textColor[colorMode]}
+          bg={bgColor[colorMode]}
         />
       </Box>
     </Flex>
