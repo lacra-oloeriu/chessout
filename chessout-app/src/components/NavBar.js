fefe9f9dc3c367b9a163b367a0d3e4208ef41d7e
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-07-26 15:04:58 +0200

    added the iconbutton

--- chessout-app/src/components/NavBar.js
+++ chessout-app/src/components/NavBar.js
@@ -2,48 +2,42 @@ import React from "react";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import {
   Box,
-  Heading,
+  //Heading,
   Flex,
   Spacer,
   useColorMode,
   IconButton,
 } from "@chakra-ui/react";
-import ImageLogo from "../assets/ImageLogo.png";
+//import ImageLogo from "../assets/ImageLogo.png";
 
 const Navbar = (props) => {
   const { colorMode, toggleColorMode } = useColorMode();
-  const bgColor = { light: "red.200", dark: "red.800" };
-  //const textColor = { light: "red.800", dark: "white" };
+  const bgColor = { light: "gray.200", dark: "gray.700" };
+  const textColor = { light: "gray.800", dark: "gray.700" };
   return (
-    <Flex minWidth="max-content" alignItems="center" gap="2">
-      <Box p="4" >
-        <Heading size="lg">
-          <IconButton
-            colorScheme="md"
-            aria-label="Call Segun"
-            size="sm"
-            background="#chakra-placeholder-color._dark"
-            icon={<img src={ImageLogo} alt="Logo" />}
-            onClick={toggleColorMode}
-          /> ChessOut
-        </Heading>
+    <Flex
+      minWidth="max-content"
+      alignItems="center"
+      gap="2"
+      bg={bgColor[colorMode]}
+    >
+      <Box p="2">
+        <Button bg={bgColor[colorMode]}>ChessOut</Button>
       </Box>
       <Spacer />
       <ButtonGroup gap="2">
-        <Button colorScheme="red">Team</Button>
-        <Button colorScheme="red">Log in</Button>
+        <Button bg={bgColor[colorMode]}>Team </Button>
+        <Button bg={bgColor[colorMode]}>Login</Button>
       </ButtonGroup>
 
-      <Box display="block" mt={{ base: 4, md: 0 }}>
-        <IconButton
-          //bg="transparent"
-          aria-label="toggle color mode"
-          icon={colorMode === "light" ? "moon" : "sun"}
-          onClick={toggleColorMode}
-          color="white"
-          bg={bgColor[colorMode]}
-        />
-      </Box>
+      <IconButton
+        //bg="transparent"
+        //aria-label="toggle color mode"
+        onClick={toggleColorMode}
+        icon={colorMode === "light" ? "moon" : "sun"}
+        color={textColor[colorMode]}
+        bg={bgColor[colorMode]}
+      />
     </Flex>
   );
 };
