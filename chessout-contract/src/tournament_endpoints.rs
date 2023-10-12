d8c1e07e9f13a60341b21b2cc2032c981b056d79
bogdan <bogdan.oloeriu@gmail.com>
2023-10-12 20:19:43 +0200

    added is_touranment_token_valid

--- chessout-contract/src/tournament_endpoints.rs
+++ chessout-contract/src/tournament_endpoints.rs
@@ -1,25 +1,18 @@
 multiversx_sc::imports!();
 
-
 use crate::data_models::ContractSettings;
 use crate::data_models::TokenSettings;
 use crate::data_models::Tournament;
 use crate::data_store;
 
-
 #[multiversx_sc::module]
-pub trait TournamentEndpoints: data_store::StoreModule{
-    
+pub trait TournamentEndpoints: data_store::StoreModule {
     #[endpoint(createTournament)]
-    fn create_tournament(&self, 
-        token_id: TokenIdentifier,
-        entry_fee: BigUint,
-        ) {
-        
+    fn create_tournament(&self, token_id: TokenIdentifier, entry_fee: BigUint) {
         let valid_token = self.is_token_valid(&token_id);
         require!(valid_token, "Token is not valid");
 
-        let id :u64 = self.increment_and_get_last_index();
+        let id: u64 = self.increment_and_get_last_index();
         let tournament_token = EgldOrEsdtTokenIdentifier::esdt(token_id);
 
         let manager = self.blockchain().get_caller();
@@ -28,18 +21,17 @@ pub trait TournamentEndpoints: data_store::StoreModule{
 
         let participant_list = ManagedVec::new();
 
-        let zero_initial_fund:  BigUint = BigUint::zero();
-        let tournament = Tournament{
+        let zero_initial_fund: BigUint = BigUint::zero();
+        let tournament = Tournament {
             id: id,
             token_id: tournament_token,
             entry_fee: entry_fee,
             available_funds: zero_initial_fund,
             manager_list: manager_list,
-            participant_list: participant_list
+            participant_list: participant_list,
         };
 
         self.tournament_data(id).set(tournament);
-        
     }
 
     fn is_token_valid(&self, token_id: &TokenIdentifier) -> bool {
@@ -59,4 +51,20 @@ pub trait TournamentEndpoints: data_store::StoreModule{
         return last_index;
     }
 
+    fn is_tournament_token_valid(&self, tournament_id: u64, token: EgldOrEsdtTokenIdentifier) -> bool {
+        let tournament = self.tournament_data(tournament_id).get();
+        if tournament.token_id == token {
+            return true;
+        }
+        return false;
+    }
+
+    #[payable("*")]
+    #[endpoint(joinTournament)]
+    fn join_tournament(&self, tournament_id: u64) {
+        let payment = self.call_value().egld_or_single_esdt();
+        let join_token = payment.token_identifier;
+        let valid_token = self.is_tournament_token_valid(tournament_id, join_token);
+        require!(valid_token, "Tournament token is not valid");
+    }
 }
--- chessout-contract/wasm/src/lib.rs
+++ chessout-contract/wasm/src/lib.rs
@@ -5,9 +5,9 @@
 ////////////////////////////////////////////////////
 
 // Init:                                 1
-// Endpoints:                            2
+// Endpoints:                            3
 // Async Callback (empty):               1
-// Total number of exported functions:   4
+// Total number of exported functions:   5
 
 #![no_std]
 
@@ -24,6 +24,7 @@ multiversx_sc_wasm_adapter::endpoints! {
         init => init
         setContractSettings => set_contract_settings
         createTournament => create_tournament
+        joinTournament => join_tournament
     )
 }
 
