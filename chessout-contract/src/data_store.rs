7a854a308990f6c766ec92697c42b8283a10bb7c
bogdan <bogdan.oloeriu@gmail.com>
2023-10-15 20:48:38 +0300

    added getMyLastTournamentId

--- chessout-contract/src/data_store.rs
+++ chessout-contract/src/data_store.rs
@@ -7,15 +7,27 @@ use crate::data_models::Tournament;
 
 #[multiversx_sc::module]
 pub trait StoreModule {
+    #[view(getContractSettings)]
     #[storage_mapper("contractSettings")]
     fn contract_settings(&self) -> SingleValueMapper<ContractSettings<Self::Api>>;
 
+    #[view(getTotalFees)]
     #[storage_mapper("totalFees")]
     fn total_fees(&self) -> SingleValueMapper<TotalFees<Self::Api>>;
 
+    #[view(getLastIndex)]
     #[storage_mapper("lastIndex")]
     fn last_index(&self) -> SingleValueMapper<u64>;
 
+    #[view(getTournamentData)]
     #[storage_mapper("tournamentData")]
     fn tournament_data(&self, id: u64) -> SingleValueMapper<Tournament<Self::Api>>;
+
+    #[view(getMyLastCreatedId)]
+    #[storage_mapper("myLastCreatedId")]
+    fn my_last_created_id(
+        &self,
+        client_address: &ManagedAddress,
+    ) -> SingleValueMapper<u64>;
+
 }
